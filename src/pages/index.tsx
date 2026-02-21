36ff1f1b1779527b44614ebd6ffce713f65d3611
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2026-02-21 17:36:28 +0700

    Membuat Komponen Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,19 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+
 //1. Import komponen link dari next/link
 import Link from 'next/link'
+import Navbar from '@/components/layouts/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div>
 
-    <Head>
-      <title>Praktikum Next.js</title>
-    </Head>  
-
-      <h1>Selamat Datang di Halaman Utama!</h1>
-      <p>Mahasiswa D4 Pengembangan Web Berbasis FrameWork</p>
-
+        <h1>Selamat Datang di Halaman Utama!</h1>
+        <p>Mahasiswa D4 Pengembangan Web Berbasis FrameWork</p>
    {/* 2. Tambahkan Navigasi ke Halaman About */}
       <nav style={{ marginTop: '20px' }}>
         <Link href="/api/about" style={{ color: 'blue', textDecoration: 'underline' }}>
