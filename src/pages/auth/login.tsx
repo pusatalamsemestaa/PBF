6ddfa7d9a62fa23fec2fcffbc75c0ad84f5e9b38
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2026-03-03 13:32:30 +0700

    Navigasi Imperatif (router.push)

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const halamanLogin = () => {
-    return (
-        <div>
-            <h1>Halaman Login</h1>
-            <Link href="/auth/register">Ke Halaman Register</Link>
-        </div>
-    );
+  const { push } = useRouter();
+
+  const handlerLogin = () => {
+    // logika login di sini
+    push('/produk');
+  };
+
+  return (
+    <div>
+      <h1>Halaman Login</h1>
+    {/*<button onClick={handlerLogin}>Login</button> <br />
+      <button onClick={() => push('/produk')}>Login</button> <br />*/}
+      <button onClick={() => handlerLogin()}>Login</button> <br />
+      <Link href="/auth/register">Ke Halaman Register</Link>
+    </div>
+  );
 };
+
 export default halamanLogin;
