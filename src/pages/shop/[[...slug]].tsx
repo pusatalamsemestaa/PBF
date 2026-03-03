05efa8ab1141badbebc675fd95156b661fce0450
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2026-03-03 12:30:03 +0700

    Optional Catch-All Route

---
+++ src/pages/shop/[[...slug]].tsx
@@ -0,0 +1,15 @@
+import Router, { useRouter } from "next/router";
+
+const halamanToko = () => {
+  //const router = useRouter();
+  //console.log(router);
+  const {query} = useRouter();
+  return (
+    <div>   
+    <h1>Halaman Toko</h1>
+    <p>Toko: {`${query.slug && query.slug[0]+"-"+query.slug[1]}`}</p>
+    <p>Kategori: {query.slug ? query.slug[0] : "Semua Kategori"}</p>
+    </div>
+    );
+};
+export default halamanToko;
