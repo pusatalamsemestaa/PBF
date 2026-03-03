577186327be65f1e02ae54174834b8c9a0d5a779
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2026-03-03 12:32:33 +0700

    Validasi Parameter

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const halamanRegister = () => {
+    return (
+        <div>
+            <h1>Halaman Register</h1>
+            <Link href="/auth/login">Ke Halaman Login</Link>
+        </div>
+    );
+};
+export default halamanRegister;
