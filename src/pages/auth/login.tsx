d433ab40db39ac974c94e96b4ae8c5f1d9054dcf
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2026-04-02 18:18:35 +0700

    Refactoring Struktur Project (Best Practice)

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,28 +1,11 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./login.module.css";
+import TampilanLogin from "@/views/auth/login";
 
 const halamanLogin = () => {
-  const { push } = useRouter();
-
-  const handlerLogin = () => {
-    // logika login di sini
-    // simulasikan penyimpanan token setelah login berhasil
-    try {
-      localStorage.setItem('token', 'dummy-token');
-    } catch (err) {
-      console.warn('Tidak bisa mengakses localStorage', err);
-    }
-    push('/produk');
-  };
-
-  return (
-    <div className={styles.login}>
-      <h1>Halaman Login</h1>
-      <button onClick={() => handlerLogin()}>Login</button> <br />
-      <Link href="/auth/register">Ke Halaman Register</Link>
-    </div>
-  );
+   return(
+  <>
+  <TampilanLogin />;
+  </>  
+    );    
 };
 
 export default halamanLogin;
